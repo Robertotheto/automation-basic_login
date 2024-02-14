2f055da28b5c50f0b3bea5f5d45616b1c7ae88a0
Roberto Dos Santos Filho <robertodossantosfilho1001@gmail.com>
2024-02-14 15:24:39 -0400

    config: initial app automation

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 load_dotenv()
-
-print(os.environ['Username'])
-print(os.environ['Password'])
 
 URL = ('https://developer.automationanywhere.com/challenges/AutomationAnywhereLabs-Login.html?'
        '_ga=2.74340240.325012646.1707265495-1646007937.1707151632&_gl=1*1t4hesj*'
@@ -13,4 +11,18 @@ URL = ('https://developer.automationanywhere.com/challenges/AutomationAnywhereLa
        '_ga_DG1BTLENXK*MTcwNzI2NTQ5NC4yLjEuMTcwNzI3MTA1Ny40NS4wLjA.')
 driver = webdriver.Chrome()
 driver.get(URL)
-sleep(30)
+sleep(10)
+button_cookie = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+button_cookie.click()
+sleep(10)
+username = driver.find_element(By.XPATH, '//*[@id="inputEmail"]')
+username.send_keys(os.environ['Username'])
+sleep(5)
+password = driver.find_element(By.XPATH, '//*[@id="inputPassword"]')
+password.send_keys(os.environ['Password'])
+sleep(5)
+button_login = driver.find_element(By.XPATH, '/html/body/main/div[1]/div/div[2]/div/div/'
+                                   'div[2]/div/form/button')
+button_login.click()
+sleep(5)
+driver.quit()
